9a4e8bde9b9b7b1b868a00e3e0110e2f801ede54
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-01-03 20:18:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,55 @@
+function getRepositories() {
+  let name = document.getElementById('username').value
+  let uri = `https://api.github.com/users/${name}/repos`
+  const xhr = new XMLHttpRequest()
+    xhr.addEventListener("load", displayRepositories)
+    xhr.open("GET", uri)
+    xhr.send()
+    return false;
+}
+
+function displayRepositories() {
+    let data = JSON.parse(this.responseText)
+    let repoList = "<ul>" + data.map(repo => {
+      let username = `data-username="${repo.owner.login}"`
+      let repoName = `data-repository="${repo.name}"`
+      return(`
+            <li>
+              <h2>${repo.name}</h2>
+              <a href="${repo.html_url}">${repo.html_url}</a><br>
+              <a href="#" ${repoName} ${username} onclick="getCommits(this)">Get Commits</a><br>
+              <a href="#" ${repoName} ${username} onclick="getBranches(this)">Get Branches</a>
+            </li>
+            `)
+  }).join('') + "</ul>";
+  document.getElementById("repositories").innerHTML = repoList
+}
+
+function getCommits(el) {
+  let repoName = el.dataset.repository
+  let uri = `https://api.github.com/repos/${el.dataset.username}/${repoName}/commits`
+  let xhr = new XMLHttpRequest()
+  xhr.addEventListener("load", displayCommits)
+  xhr.open("GET", uri)
+  xhr.send()
+}
+
+function displayCommits() {
+  let commits = JSON.parse(this.responseText)
+  let commitsList = `<ul>${commits.map(commit =>
+                        <li>
+                            <h3>
+                              ${commit.commit.author.name}(${commit.author.login})
+                            </h3>
+                          ${commit.commit.message}
+                        </li>).join('')}
+                    </ul>`
+
+    document.getElementById("details").innerHTML = commitsList
+}
+function getBranches() {
+
+}
+function displayBranches() {
+
+}
